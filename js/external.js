6c31aa8095efb218c75368f16001de8fe91f477a
Marissa Rodriguez <rodr.marissa@gmail.com>
2020-07-14 09:30:25 -0500

    added js alerts and prompts

--- js/external.js
+++ js/external.js
@@ -1,3 +1,9 @@
 "use strict";
 
 console.log ("Hello from external Javascript");
+
+alert("Welcome to my website!");
+
+var userInput = prompt('What is your favorite color?');
+console.log('The user entered: ' + userInput);
+alert('Great, ' + userInput + ' is my favorite color, too!');
